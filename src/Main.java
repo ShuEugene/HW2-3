e8714df7c2196a7aa705f25ad612f0625aad3f42
ShuEugene <shueugene@hotmail.com>
2022-08-25 13:58:12 +0300

    HW2-3.1 complete.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,20 @@
 public class Main {
     public static void main(String[] args) {
+
+        byte b = 1;
+        System.out.println();
+        System.out.println("Значение переменной b с типом byte равно " + b + ".");
+        short s = 2;
+        System.out.println("Значение переменной s с типом short равно " + s + ".");
+        int i = 3;
+        System.out.println("Значение переменной i с типом int равно " + i + ".");
+        long l = 4L;
+        System.out.println("Значение переменной l с типом long равно " + l + ".");
+        float f = 5f;
+        System.out.println("Значение переменной f с типом float равно " + f + ".");
+        double d = 6;
+        System.out.println("Значение переменной d с типом double равно " + d + ".");
+
+
     }
 }
